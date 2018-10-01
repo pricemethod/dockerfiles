9b68c70c560e6613fe2f137376f2173c4593f572
Forest Belton <forest.belton@staylyric.com>
2018-10-01 13:57:48 -0700

    Move build directory to named build target directory

--- js-web/scripts/build.sh
+++ js-web/scripts/build.sh
@@ -8,5 +8,6 @@ cp .env.production .env.production.bak
 cp ".env.$TARGET" .env.production
 
 ./node_modules/.bin/react-scripts build
+mv build "build-$TARGET"
 
 mv .env.production.bak .env.production
